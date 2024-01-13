21a83b8cab9455ccd766174f94165751c1207fdb
Kdata249 <karelklos249@gmail.com>
2024-01-13 12:26:25 -0500

    done

--- static/js/app.js
+++ static/js/app.js
@@ -9,8 +9,12 @@ function init() {
             dropdown.append("option").text(sample).property("value", sample);
         }
     
+        console.log(data)
+        
         let initialSample = data.names[0];
         updateBarChart(initialSample);
+        updateBubbleChart(initialSample);
+        updateDemoData(initialSample);
     })
 }
 
@@ -35,23 +39,74 @@ function updateBarChart(selectedSample){
         };
 
         Plotly.newPlot("bar", barData, barLayout);
-
-        console.log(data)
     })
 
 }
 
-function updateDemoData(selectedSample){
+function updateBubbleChart(selectedSample) {
+    // URL of the JSON file
+    const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+    // Select the div where the bubble chart will be displayed
+    let bubbleChartDiv = d3.select('#bubble');
+    
+    // Clear existing content in the bubble chart div
+    bubbleChartDiv.html('');
+
+    // Fetch the data from the URL
     d3.json(url).then(function(data) {
-        let selectedData = data.metadata.find(metadata => metadata.id == selectedSample);
-        let demoBox = d3.select('#panel-title');
-        demoBox.append('')
-    })
+        // Find the sample data for the selected sample
+        let selectedSampleData = data.samples.find(sample => sample.id == selectedSample);
+
+        // Create the bubble chart
+        var trace = {
+            x: selectedSampleData.otu_ids,
+            y: selectedSampleData.sample_values,
+            text: selectedSampleData.otu_labels,
+            mode: 'markers',
+            marker: {
+                size: selectedSampleData.sample_values,
+                color: selectedSampleData.otu_ids,
+                colorscale: 'Viridis'
+            }
+        };
+
+        var layout = {
+            title: `Bubble Chart for Sample ${selectedSample}`,
+            xaxis: { title: 'OTU IDs' },
+            yaxis: { title: 'Sample Values' }
+        };
+
+        Plotly.newPlot('bubble', [trace], layout);
+    });
+}
+
+function updateDemoData(selectedSample) {
+    // URL of the JSON file
+    const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+    // Select the panel body to display demographic info
+    let demoBox = d3.select('#sample-metadata');
+
+    // Clear existing content in the demo box
+    demoBox.html('');
+
+    // Fetch the data from the URL
+    d3.json(url).then(function(data) {
+        // Find the metadata for the selected sample
+        let selectedMetadata = data.metadata.find(metadata => metadata.id == selectedSample);
+
+        // Display demographic info
+        Object.entries(selectedMetadata).forEach(([key, value]) => {
+            demoBox.append('p').text(`${key}: ${value}`);
+        });
+    });
 }
 
 function optionChanged(selectedSample){
     updateBarChart(selectedSample);
-    updateDemoData(selectedSample);
+    updateBubbleChart(selectedSample);
+    updateDemoData(selectedSample)
 }
 
 init();
